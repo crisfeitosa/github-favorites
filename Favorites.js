002c97360920dd8100579fb03a5efa3aaa5b407c
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-16 17:44:48 -0300

    Cuidando do fluxo da aplicação com try, catch, throw

--- Favorites.js
+++ Favorites.js
@@ -27,8 +27,24 @@ export class Favorites {
     this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || [];
   };
 
+  save() {
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries));
+  }
+ 
   async add(username) {
-    const user = await GithubUser.search(username);
+    try {
+      const user = await GithubUser.search(username);
+  
+      if(user.login === undefined) {
+        throw new Error('Usuário não encontrado!');
+      }
+  
+      this.entries = [user, ...this.entries];
+      this.update();
+      this.save();
+    } catch(error) {
+      alert(error.message);
+    }
   }
   
   delete(user) {
@@ -36,6 +52,7 @@ export class Favorites {
 
     this.entries = filteredEntries;
     this.update();
+    this.save();
   }
 }
 
