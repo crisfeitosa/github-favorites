10f7b9ef3f5427e89921de3c8f404f5c124415c6
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-16 12:17:49 -0300

    Criando HTML com JavaScript

--- Favorites.js
+++ Favorites.js
@@ -1,18 +1,73 @@
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root);
+    this.load();
   }
+
+  load() {
+    this.entries = [{
+      login: 'maykbrito',
+      name: 'Mayk Brito',
+      public_repos: '76',
+      followers: '9589'
+    },
+    {
+      login: 'diego3g',
+      name: 'Diego Fernandes',
+      public_repos: '48',
+      followers: '22503'
+    },
+  ]};
 }
 
 export class FavoritesView extends Favorites {
   constructor(root) {
     super(root);
 
+    this.tbody = this.root.querySelector('table tbody');
+
     this.update();
   }
 
   update() {
     this.removeAllTr();
+
+    this.entries.forEach(user => {
+      const row = this.createRow();
+      
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+      this.tbody.append(row);
+    });
+  }
+
+  createRow() {
+    const tr = document.createElement('tr');
+      tr.innerHTML = `
+        <td class="user">
+          <img src="https://github.com/crisfeitosa.png" alt="Imagem de crisfeitosa">
+          <a href="https://github.com/crisfeitosa" target="_blank">
+            <p>Cristiano Feitosa</p>
+            <span>crisfeitosa</span>
+          </a>
+        </td>
+        <td class="repositories">
+          76
+        </td>
+        <td class="followers">
+          9589
+        </td>
+        <td>
+          <button class="remove">&times;</button>
+        </td>
+      `;
+
+    return tr;
   }
 
   removeAllTr() {
